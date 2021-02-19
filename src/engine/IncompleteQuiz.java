2d9d0d16bcf9f04cc09c3af58d82a47a191b271b
RealThomasMiles <dmilya2000@gmail.com>
2021-02-19 13:25:33 +0300

    Added bean validation

--- src/engine/IncompleteQuiz.java
+++ src/engine/IncompleteQuiz.java
@@ -1,17 +1,24 @@
 package engine;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class IncompleteQuiz {
+    @NotEmpty(message = "The title is required")
     private String title;
+    @NotEmpty(message = "The text is required")
     private String text;
+    @NotNull
+    @Size(min = 2, message = "At least 2 options are required")
     private String[] options;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private int answer;
+    private ArrayList<Integer> answer;
 }
