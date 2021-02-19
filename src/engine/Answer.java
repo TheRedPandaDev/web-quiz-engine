507cd960f284ff12e9d6bba4ec55f6b1304346fb
RealThomasMiles <dmilya2000@gmail.com>
2021-02-19 13:26:16 +0300

    Added multiple option answer support

---
+++ src/engine/Answer.java
@@ -0,0 +1,14 @@
+package engine;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Answer {
+    List<Integer> answer;
+}
